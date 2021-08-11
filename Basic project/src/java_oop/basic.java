0bd61ed8fda64e3562dd48988dd664f52fcc3b27
kratikashrivastva <kratika.shrivastva@sunrays.co.in>
2021-08-11 20:24:30 +0530

    test 1

---
+++ Basic project/src/java_oop/basic.java
@@ -0,0 +1,7 @@
+package java_oop;
+
+public class basic {
+   public static void main(String[]args){
+	System.out.println("basic");
+}
+}
---
+++ Basic project/src/java_oop/views_java.java
@@ -0,0 +1,7 @@
+package java_oop;
+
+public class ADDISION {
+public static void main(String[]args) {
+	
+}
+}
